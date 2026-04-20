2799d1a4100a8ae95562912a38d80f40a821f6e1
shivanshuGautam <harigautam772@gmail.com>
2026-04-21 00:18:30 +0530

    Add initial HTML and JavaScript files for basic webpage structure

---
+++ jsbasic/script.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+console.log("Shivanshu gautam");
+console.log("vip number");
+
+let a = 5;
+
+a = 10;
+
+let b = true;
+
+const pi = 3.14;
+
+
+console.log("task complete");
+
+let x = 10;
+let y = "10";
+
+console.log(typeof x);
+console.log(typeof y);
+
+
+console.log(x == y);
+
+console.log(x === y);
+
