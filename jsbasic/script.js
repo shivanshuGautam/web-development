7936c8bcbc75ca3adf0bdbe8e7dd9e4127d9cbaa
shivanshuGautam <harigautam772@gmail.com>
2026-04-21 00:42:05 +0530

    Add additional console logs for debugging and average calculation

--- jsbasic/script.js
+++ jsbasic/script.js
@@ -1,6 +1,7 @@
 console.log("Hello World!");
 console.log("Shivanshu gautam");
 console.log("vip number");
+console.log("divakar gautam");
 
 let a = 5;
 
@@ -24,3 +25,15 @@ console.log(x == y);
 
 console.log(x === y);
 
+let age = 25;
+console.log(typeof age);
+
+let shiva = "shivanshu";
+console.log(typeof shiva);
+
+let hindi = 26;
+let english = 45;
+let maths = 45;
+let bio = 48;
+let avj = (hindi + english + maths + bio) / 4;
+console.log(avj);
