a573223c47f1f8d4de60127be8ba1628479ce264
shivanshuGautam <harigautam772@gmail.com>
2026-04-29 23:13:08 +0530

    Add HTML structure and JavaScript for looping examples
    - Created index.html with basic HTML structure and linked script.js
    - Added a for loop to log even numbers from 0 to 20
    - Included examples of for, while, and do-while loops to demonstrate iteration over an array and number sequences

---
+++ basicclass/script.js
@@ -0,0 +1,32 @@
+for(var i=0; i<=10; i++)
+{
+    console.log(i*2);
+}
+
+let a = [24,58,33,86,96,12,53,23,32,100]
+// for(var i =0; i<a.length; i++)
+// {
+//     console.log(a[i]);
+// }
+
+// var i=0;
+// while(i<10)
+// {
+//     console.log(a[i]);
+//     i++;
+// }
+
+// do-while
+ var i=1
+do{
+      console.log(i*2);
+      i++
+}while(i<=10);
+
+// while
+var i=1
+while(i<=10)
+{
+      console.log(i*2);
+      i++
+}
