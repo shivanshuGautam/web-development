6f7235601daeb3c4ff034caa2abfb23120364ad5
shivanshuGautam <harigautam772@gmail.com>
2026-04-30 16:54:40 +0530

    Add new HTML and JavaScript files for iteration examples using for and while loops

---
+++ jspassignment/iterationassgin/whileloop/script.js
@@ -0,0 +1,31 @@
+// 1.Write a program to print numbers from 1 to 5 using a while loop.
+let j = 1;
+while (j <= 5) {
+    console.log(j);
+    j++;
+}
+
+// 2. Write a program to print numbers from 5 to 1 using a while loop.
+let k = 5;
+while (k >= 1) {
+    console.log(k);
+    k--;
+}
+
+ // 3. Print numbers starting from 2 and keep adding 2 while less than 11
+ let num = 2;
+ while(num<11)
+ {
+    console.log(num);
+    num += 2;
+ }
+
+// //  4. Write a program to calculate the sum of  numbers from 10 to 5 using a while loop
+let i = 10;
+let sum = 0; 
+while(i >= 5) {
+    sum += i;
+    console.log(i);
+    i--; 
+}
+console.log("Sum =", sum);
