800ff8441c7de31ed4f9c474a0eb50003e6614b5
shivanshuGautam <harigautam772@gmail.com>
2026-04-30 19:35:38 +0530

    Add additional conditional statements examples and fix typos in login messages

--- jsconditionalstatement/script.js
+++ jsconditionalstatement/script.js
@@ -22,3 +22,43 @@ if (num % 2 == 0) {
 } else {
   console.log("even not");
 }
+
+// question no.4
+let number = 56;
+if (number > 90) {
+  console.log("A");
+} else if (number > 75 && number <= 89) {
+  console.log("b");
+} else if (number > 50 && number <= 74) {
+  console.log("b");
+} else {
+  console.log("fail");
+}
+
+// question no.5
+
+let user = "abcde";
+let pass = "12345";
+if (user === "abcde" && pass === "12345") {
+  console.log("login sucess");
+} else {
+  console.log("login faild");
+}
+
+// question no.6
+let data;
+let name1 = "shivanshu";
+data = name1 || "n/a"
+console.log(data);
+// if(name1)
+// {
+//     data = name1;
+// }
+// else{
+//     data = "n/a"
+// }
+
+
+// question .7 
+ let x = 2;
+ console.log(x>10 && y<5);
