b791964574bc4da4fe613729b198c674611cbdbe
shivanshuGautam <harigautam772@gmail.com>
2026-04-30 18:56:05 +0530

    Add initial HTML and JavaScript files for conditional statements examples

---
+++ jsconditionalstatement/script.js
@@ -0,0 +1,24 @@
+// question no,1
+let n = 5;
+if (n > 0) {
+  console.log("postive Integer");
+} else {
+  console.log("negative Integer");
+}
+
+// question n0.2
+
+let age = 15;
+if (age >= 18) {
+  console.log("you can vote");
+} else {
+  console.log("sorry");
+}
+
+// question no.3
+let num = 5;
+if (num % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("even not");
+}
