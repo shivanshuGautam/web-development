0fddf5773d4078577adbfaf37f13423bed82f1e0
shivanshuGautam <harigautam772@gmail.com>
2026-05-01 19:41:12 +0530

    Add initial HTML and JavaScript files for arrow function examples

---
+++ javascriptfunction/Arrowfunction/script.js
@@ -0,0 +1,18 @@
+const arrowfun = () => {
+  console.log("i am arrow function");
+};
+
+arrowfun();
+
+
+// add function addarrow
+const addarrow = (a,b) => 
+{
+    return a+b ;
+}
+
+console.log(addarrow(5,6));
+// or
+
+const addaarrow = (a,b) =>  a+b;
+console.log(addaarrow(6,6));
