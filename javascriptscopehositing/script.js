b4b7f00df0e7922cc19658757b05fe517d0df99a
shivanshuGautam <harigautam772@gmail.com>
2026-05-02 18:58:22 +0530

    Add initial HTML structure and JavaScript functions for scope and hoisting demonstration

---
+++ javascriptscopehositing/script.js
@@ -0,0 +1,41 @@
+let a = 5;
+console.log(a);
+console.log(b);
+
+var b = 7;
+console.log(a);
+console.log(b);
+
+let test = "this is a global variable";
+
+function classA() {
+  console.log("classA", test);
+}
+
+function classb() {
+  console.log("classb", test);
+}
+
+function classc() {
+  let test2 = "this is a local variable";
+  console.log("classc", test);
+  console.log("classc", test2);
+}
+
+function classd() {
+  console.log("classd", test);
+  {
+    let v =10;
+    console.log(v);
+  }
+}
+
+function classe() {
+  console.log("classe", test);
+}
+
+classA();
+classb();
+classc();
+classd();
+classe();
