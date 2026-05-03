8b5ea9a3d0858c9236937c73f239b441e253bc6e
shivanshuGautam <harigautam772@gmail.com>
2026-05-03 15:24:53 +0530

    Comment out hoisting demonstration code in script2.js for clarity

--- javascriptscopehositing/script2.js
+++ javascriptscopehositing/script2.js
@@ -1,28 +1,39 @@
-//  Hoisting
+// //  Hoisting
 
-let a;
-let b;
-let c;
+// let a;
+// let b;
+// let c;
 
-a = 10;
+// a = 10;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-b = 15;
-console.log(a);
-console.log(b);
-console.log(c);
+// b = 15;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-c = 16;
-console.log(a);
-console.log(b);
-console.log(c);
+// c = 16;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-// function
+// // function
 
-abc();
-function abc() {
-  console.log("i am a function");
+// abc();
+// function abc() {
+//   console.log("i am a function");
+// }
+
+var a = 10;
+var b = 20;
+
+function addnum(num1, num2) {
+  var sum = num1 + num2;
+  return sum;
 }
+var sumresult1 = addnum(a, b);
+var sumresult2 = addnum(4, 5);
+console.log(sumresult1, sumresult2);
