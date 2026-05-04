177068ebe68ddf1540f7488f0e8a9e225c82a573
shivanshuGautam <harigautam772@gmail.com>
2026-05-04 20:58:49 +0530

    Add index.html, script.js, and style.css for DOM manipulation example

---
+++ jsdom/script.js
@@ -0,0 +1,13 @@
+console.log("Dom-document object model");
+function submit() {
+  console.log("submit button clicked");
+
+  const fn = document.getElementById("fullname").value;
+  console.log(fn);
+
+  document.getElementById("mydata").innerText = fn;
+  document.getElementById("fullname").value = "";
+
+  document.getElementById("datacard").classList.add("divShow");
+   document.getElementById("datacard").classList.add("divHide");
+}
