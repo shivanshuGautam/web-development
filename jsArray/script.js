6d98e6450946849e05e6e88f7e0e738593914aa8
shivanshuGautam <harigautam772@gmail.com>
2026-05-05 19:24:36 +0530

    Refactor array manipulation examples to include push, pop, unshift, and shift operations

--- jsArray/script.js
+++ jsArray/script.js
@@ -13,3 +13,22 @@ console.log(arr);
 //  delete
 arr = [];
 console.log(arr);
+
+// push
+let arr = [12, 89, 76, 56, 45, 89, 54, 34, 78, 90];
+console.log(arr);
+arr.push(99);
+console.log(arr);
+
+// pop
+let arr1 = [12, 89, 76, 56, 45, 89, 54, 34, 78, 90];
+arr.pop();
+console.log(arr);
+
+// unshift
+arr.unshift(11);
+console.log(arr);
+
+// shift
+arr.shift();
+console.log(arr);
