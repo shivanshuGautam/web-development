eea35fe4d2682af66eef9f2c0f501a6ee761bd96
shivanshuGautam <harigautam772@gmail.com>
2026-05-05 19:17:15 +0530

    Add initial HTML and JavaScript files for array manipulation example

---
+++ jsArray/script.js
@@ -0,0 +1,15 @@
+let arr = [12, 13, 45, 67, 89, 34];
+for (var i = 0; i < 6; i++) {
+  console.log(arr[i]);
+}
+
+console.log(arr);
+
+//  update
+
+arr[2] = arr[2] + 10;
+console.log(arr);
+
+//  delete
+arr = [];
+console.log(arr);
