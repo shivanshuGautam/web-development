ec204d60f4a765929d5570ef015909afbe0a1a16
shivanshuGautam <harigautam772@gmail.com>
2026-05-05 19:44:45 +0530

    Refactor script.js to comment out previous array operations and enhance readability

--- jsArray/script.js
+++ jsArray/script.js
@@ -1,34 +1,47 @@
+// // initial array
+// let arr = [12, 13, 45, 67, 89, 34];
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+// console.log(arr);
+
+// // update
+// arr[2] = arr[2] + 10;
+// console.log(arr);
+
+// // delete (empty array)
+// arr = [];
+// console.log(arr);
+
+// // push
+// arr = [12, 89, 76, 56, 45, 89, 54, 34, 78, 90];
+// console.log(arr);
+// arr.push(99);
+// console.log(arr);
+
+// // pop
+// arr.pop();
+// console.log(arr);
+
+// // unshift
+// arr.unshift(11);
+// console.log(arr);
+
+// // shift
+// arr.shift();
+// console.log(arr);
+
+// short
+
+// let unar = [12.67, 65, 9, 43, 65, 73, 24];
+// unar.sort;
+// console.log(unar);
+
 let arr = [12, 13, 45, 67, 89, 34];
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
-
-console.log(arr);
-
-//  update
-
-arr[2] = arr[2] + 10;
-console.log(arr);
-
-//  delete
-arr = [];
-console.log(arr);
-
-// push
-let arr = [12, 89, 76, 56, 45, 89, 54, 34, 78, 90];
-console.log(arr);
-arr.push(99);
-console.log(arr);
-
-// pop
-let arr1 = [12, 89, 76, 56, 45, 89, 54, 34, 78, 90];
-arr.pop();
-console.log(arr);
-
-// unshift
-arr.unshift(11);
-console.log(arr);
-
-// shift
-arr.shift();
-console.log(arr);
+console.log("for each");
+arr.forEach((elements) => {
+  console.log(elements);
+});
