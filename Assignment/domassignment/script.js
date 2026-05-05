674f6b0c53390457326d2ba3b30c0976207316e1
shivanshuGautam <harigautam772@gmail.com>
2026-05-05 18:23:59 +0530

    Add email and password input fields, update submit function to capture additional data

--- Assignment/domassignment/script.js
+++ Assignment/domassignment/script.js
@@ -21,11 +21,17 @@ function submit() {
   const fn = document.getElementById("fullname").value;
   console.log(fn);
 
+  const fn1 = document.getElementById("email1").value;
+  console.log(fn1);
+
+  const fn2 = document.getElementById("city").value;
+   console.log(fn2);
+
+    const fn3 = document.getElementById("password1").value;
+   console.log(fn3);
+   
   document.getElementById("mydata").innerText = fn;
 }
-
-
-
 
 //    <div class=" border rounded shadow w-25 p-2 divHide" id="datacard">
 //         <span>Name:</span>
