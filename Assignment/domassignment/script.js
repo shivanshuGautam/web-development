4f25bfcb071428099b8b410e9a0d3f7198bf934a
shivanshuGautam <harigautam772@gmail.com>
2026-05-05 13:59:21 +0530

    Refactor index.html and script.js for improved structure and functionality

--- Assignment/domassignment/script.js
+++ Assignment/domassignment/script.js
@@ -1,3 +1,31 @@
 const tempp = document.getElementById("secondname");
 console.log(tempp.innerText);
 console.log(tempp);
+
+// function submit() {
+//   console.log("submit button clicked");
+
+//   const fn = document.getElementById("fullname").value;
+//   console.log(fn);
+
+//   document.getElementById("mydata").innerText = fn;
+//   document.getElementById("fullname").value = "";
+
+//   document.getElementById("datacard").classList.add("divShow");
+//    document.getElementById("datacard").classList.remove("divHide");
+// }
+
+function submit() {
+  console.log("Submit button clicked");
+
+  const fn = document.getElementById("fullname").value;
+  console.log(fn);
+}
+
+
+
+
+//    <div class=" border rounded shadow w-25 p-2 divHide" id="datacard">
+//         <span>Name:</span>
+//       <span id="mydata"></span>
+//     </div>
