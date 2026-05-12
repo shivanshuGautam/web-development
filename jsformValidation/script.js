0fccb1eaed444e848c41e8723d5f7f49e49786fc
shivanshuGautam <harigautam772@gmail.com>
2026-05-12 19:20:29 +0530

    Add HTML and JavaScript files for form validation functionality

---
+++ jsformValidation/script.js
@@ -0,0 +1,13 @@
+function submit() {
+  const fn = document.getElementById("fullname").value;
+  const fn1 = document.getElementById("phone").value;
+  const fn2 = document.getElementById("email").value;
+  const fn3 = document.getElementById("password").value;
+
+  console.log(fn);
+  console.log(fn1);
+  console.log(fn2);
+  console.log(fn3);
+
+
+}
