edf9277519de811c0ae55408d7ce66248b7895ae
shivanshuGautam <harigautam772@gmail.com>
2026-05-14 12:47:12 +0530

    Add initial HTML and JavaScript files for operators demonstration

---
+++ jspratice/operators/script.js
@@ -0,0 +1,17 @@
+// Aassignment Operator
+
+let a = 13;
+a += 1;
+console.log(a);
+
+// let a =90;
+// let c = 5;
+// let d = a+c;
+// console.log(d);
+
+
+let b = 20;
+ b -= 5;
+ console.log(b);
+
+//  uniary operators
