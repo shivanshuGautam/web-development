5586e94d4c686eec482426c42fc1689f9cbe1a3a
shivanshuGautam <harigautam772@gmail.com>
2026-05-14 07:30:59 +0530

    Add HTML and JavaScript files for basic operation demonstration

---
+++ jspratice/operation/script.js
@@ -0,0 +1,18 @@
+/* operation */
+/* Operator	Meaning
++	       Addition
+-	      Subtraction
+*	     Multiplication
+/	     Division
+%	     Modulus (remainder) */
+// **       exponentation
+
+let a = 10;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
