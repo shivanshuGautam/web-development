9e532ca75304f2f0ff5941aa727f60725808d448
shivanshuGautam <harigautam772@gmail.com>
2026-05-15 18:50:07 +0530

    Refactor HTML structure and update CSS for personal fieldset styling

--- jspratice/operators/script.js
+++ jspratice/operators/script.js
@@ -1,3 +1,9 @@
+// Difference Between Assignment and Unary Operators
+// Assignment Operator	          Unary Operator
+// Value assign karta hai	      Single value par operation karta hai
+// Example: = +=	             Example: ++ -- typeof
+// Mostly 2 operands	         Sirf 1 operand
+
 // Aassignment Operator
 
 let a = 13;
@@ -9,9 +15,20 @@ console.log(a);
 // let d = a+c;
 // console.log(d);
 
-
 let b = 20;
- b -= 5;
- console.log(b);
+b -= 5;
+console.log(b);
 
 //  uniary operators
+
+// incriment
+let c = 100;
+c++;
+console.log(c++);
+// decriment
+let d = 101;
+d--;
+console.log(d--);
+
+// preincriment a++
+// post incriment ++a;
