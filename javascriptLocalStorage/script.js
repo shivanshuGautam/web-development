0a2e37a31aeebb8cbfc30680bac095fbd625d4f9
shivanshuGautam <harigautam772@gmail.com>
2026-05-18 03:27:11 +0530

    form submission to display user data and update local storage handling

--- javascriptLocalStorage/script.js
+++ javascriptLocalStorage/script.js
@@ -13,12 +13,13 @@
 // localStorage.setItem("name", JSON.stringify(user));
 
 let form = document.querySelector("form");
+let main = document.querySelector(".main");
 form.addEventListener("submit", (event) => {
   let name = event.target.Name.value;
   let email1 = event.target.email.value;
   let phone = event.target.phone.value;
 
-  var userdata = JSON.parse(localStorage.getItem("UserDetails")) ?? [];
+  let userdata = JSON.parse(localStorage.getItem("UserDetails")) ?? [];
   userdata.push({
     name: name,
     email: email1,
@@ -26,8 +27,30 @@ form.addEventListener("submit", (event) => {
   });
 
   localStorage.setItem("UserDetails", JSON.stringify(userdata));
+  displayData();
   //   console.log(userdata);
   //    console.log(name, email1, phone);
 
   event.preventDefault();
 });
+
+let displayData = () => {
+  let userdata = JSON.parse(localStorage.getItem("UserDetails")) ?? [];
+  let finaldata = "";
+  userdata.forEach((element, i) => {
+    finaldata += `<div class="items">
+        <span>&times;</span>
+        <h5>Name:</h5>
+        <div>${element.name}</div>
+
+        <h5>Email:</h5>
+        <div>${element.email}</div>
+
+        <h5>Phone number:</h5>
+         <div>${element.phonenumber}</div>
+      </div>
+    `;
+  });
+
+  main.innerHTML = finaldata;
+};
