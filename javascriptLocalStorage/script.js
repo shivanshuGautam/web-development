40754c5a8dcc495216411654baeaae33ec3d6cc7
shivanshuGautam <harigautam772@gmail.com>
2026-05-17 19:54:44 +0530

    Implement form submission handling with console logging for user input

--- javascriptLocalStorage/script.js
+++ javascriptLocalStorage/script.js
@@ -1,4 +1,10 @@
-// let form = document.querySelector("form");
-// form.addEventListener("submit",(event)=>{
-//     alert("hello")
-// })
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  let name = event.target.Name.value;
+  let email1 = event.target.email.value;
+  let phone = event.target.phone.value;
+  console.log(name);
+  console.log(email1);
+  console.log(phone);
+  event.preventDefault();
+});
