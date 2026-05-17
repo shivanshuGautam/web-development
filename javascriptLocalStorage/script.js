dc0cd81d4e6b3ec343338cd4a495136b0e148ba9
shivanshuGautam <harigautam772@gmail.com>
2026-05-18 02:44:13 +0530

    Add required attributes to form inputs for validation

--- javascriptLocalStorage/script.js
+++ javascriptLocalStorage/script.js
@@ -1,10 +1,33 @@
+// let user = [
+//   {
+//     name: "ws",
+//     email: "shivanshu@1234",
+//   },
+
+//   {
+//     name1: "ws1",
+//     email: "shivanshu@12345",
+//   },
+// ];
+
+// localStorage.setItem("name", JSON.stringify(user));
+
 let form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   let name = event.target.Name.value;
   let email1 = event.target.email.value;
   let phone = event.target.phone.value;
-  console.log(name);
-  console.log(email1);
-  console.log(phone);
+
+  var userdata = JSON.parse(localStorage.getItem("UserDetails")) ?? [];
+  userdata.push({
+    name: name,
+    email: email1,
+    phonenumber: phone,
+  });
+
+  localStorage.setItem("UserDetails", JSON.stringify(userdata));
+  //   console.log(userdata);
+  //    console.log(name, email1, phone);
+
   event.preventDefault();
 });
