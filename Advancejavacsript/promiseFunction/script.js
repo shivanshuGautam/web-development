25816d1dc90075254215d23a198d29354c19fb77
shivanshuGautam <harigautam772@gmail.com>
2026-05-19 19:54:36 +0530

    getData function to be asynchronous and update script execution method

--- Advancejavacsript/promiseFunction/script.js
+++ Advancejavacsript/promiseFunction/script.js
@@ -1,4 +1,4 @@
-function getData(value) {
+ async function getData(value) {
   return new Promise((resolve, regected) => {
     setTimeout(() => {
       if (value === 3) {
@@ -11,9 +11,20 @@ function getData(value) {
   });
 }
 
-getData(1)
-  .then(() => getData(2))
-  .then(() => getData(3))
-  .then(() => getData(4))
-  .then(() => getData(5))
-  .catch((rej) => console.log(rej));
+// promsicallback
+// getData(1)
+//   .then(() => getData(2))
+//   .then(() => getData(3))
+//   .then(() => getData(4))
+//   .then(() => getData(5))
+//   .catch((rej) => console.log(rej));
+
+
+// Asynecallback
+
+await getData(1)
+await getData(2)
+await getData(3)
+await getData(4)
+await getData(5)
+await getData(6)
