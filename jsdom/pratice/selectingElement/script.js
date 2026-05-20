20f96067728e6ebc506eb8f0eecc71a5f4ec5788
shivanshuGautam <harigautam772@gmail.com>
2026-05-20 23:54:07 +0530

    Refactor HTML structure and improve JavaScript comments for clarity

--- jsdom/pratice/selectingElement/script.js
+++ jsdom/pratice/selectingElement/script.js
@@ -2,7 +2,7 @@
 
 // Method	                     Syntax	                                                             Description
 // By ID    	   document.getElementById("id")	               1.   Ek specific element ko uske unique id se select karta hai
-// By Tag Name	 document.getElementsByTagName("tag")	          2.  Saare elements select karta hai jo ek particular HTML tag ke hain
+// By Tag Name	  document.getElementsByTagName("tag")	          2.  Saare elements select karta hai jo ek particular HTML tag ke hain
 // By Class Name	document.getElementsByClassName("class")	  3.  Saare elements select karta hai jo ek particular class ke hain
 // Query Selector	document.querySelector("selector")	          4. Pehla element select karta hai jo CSS selector match karta hai
 // Query Selector All	document.querySelectorAll("selector")	  5. Saare elements select karta hai jo CSS selector match karte hain
@@ -15,6 +15,12 @@ console.log(headingElement.innerText);
 console.log(headingElement.innerHTML);
 resElement.innerHTML = headingElement.innerHTML;
 
+// 2.document.getElementsByTagName("tag")
 
-// 2.document.getElementsByTagName("tag")	
+let tagElement = document.getElementsByTagName("p");
+console.log(tagElement[0].innerHTML);
 
+// 3.	document.getElementsByClassName("class")
+
+let classElement = document.getElementsByClassName("main");
+console.log(classElement[0].innerText);
