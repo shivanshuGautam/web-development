0f8cacfada14387e4fa48e355a9c0cf060704505
shivanshuGautam <harigautam772@gmail.com>
2026-05-23 20:45:53 +0530

    Add onclick event to button for joke retrieval and update GetNewJoke function

--- projects/jokeApp/script.js
+++ projects/jokeApp/script.js
@@ -1,9 +1,25 @@
-async function GetNewJoke() {
-  const API_URL = "https://v2.jokeapi.dev/joke/Any";
+// async function GetNewJoke() {
+//   const API_URL = "https://official-joke-api.appspot.com/jokes/random";
 
-  const Response = await fetch(API_URL);
-  const data = await Response.json();
+//   const Response = await fetch(API_URL);
+//   const data = await Response.json();
 
-  document.getElementById("setup").innerText = data.joke || data.setup;
-  document.getElementById("delivery").innerText = data.delivery || "";
-}
+//   document.getElementById("setup").innerText = data.joke || data.setup;
+//   document.getElementById("delivery").innerText = data.delivery || "";
+// }
+
+
+async function GetNewJoke()
+    {
+        const API_URL = "https://v2.jokeapi.dev/joke/Any"
+        const response = await fetch(API_URL);
+
+        console.log(response);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        document.getElementById("setup").innerText = data.joke || data.setup;
+        document.getElementById("delivery").innerText = data.delivery || "";
+    }
