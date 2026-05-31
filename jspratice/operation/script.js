c45733263c2fdc56810e0a4c48862ff74596634d
shivanshuGautam <harigautam772@gmail.com>
2026-05-31 23:22:50 +0530

    Add logical operations examples and fix variable reference in script.js

--- jspratice/operation/script.js
+++ jspratice/operation/script.js
@@ -16,3 +16,19 @@ console.log(a / b);
 console.log(a % b);
 console.log(a ** b);
 
+// 
+let a1 = 10, b2 = 3;
+console.log(a1 + b1);  // 13
+console.log(a1 % b1);  // 1
+
+
+// Logical Operations
+// && → AND
+
+// || → OR
+
+// ! → NOT
+
+javascript
+let age = 20;
+console.log(age > 18 && age < 30); // true
