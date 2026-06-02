bee29184928b7fb347e5dcf9550c2475cdc9b490
shivanshuGautam <harigautam772@gmail.com>
2026-06-02 23:10:19 +0530

    Add additional page imports in App.jsx for ContactsUs, Products, Login, and Registers

--- Frontend/myFirstReactApp/src/App.jsx
+++ Frontend/myFirstReactApp/src/App.jsx
@@ -2,6 +2,11 @@ import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import ContactsUs from "./Pages/ContactsUs";
+import products from "./Pages/Products";
+import login from "./Pages/Login";
+import registers from "./Pages/Registers";
+import {BrowserRouter, Routes,Rotee } from "react-router-dom";
 
 function App() {
   return (
