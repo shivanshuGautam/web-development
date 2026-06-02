0965a36899359b0964143f5f51725ea980d60f1c
shivanshuGautam <harigautam772@gmail.com>
2026-06-02 23:13:55 +0530

    Refactor Header component to use React Router Links and improve layout

--- Frontend/myFirstReactApp/src/Component/Header.jsx
+++ Frontend/myFirstReactApp/src/Component/Header.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
 
+import { Link } from "react-router-dom";
 function Header() {
   return (
-    <div>
-      <div>My Company</div>
-      <div>
-        <span>Home</span>
-        <span>About</span>
-        <span>ContactUs</span>
-      </div>
-      <div></div>
-    </div>
-  )
+    <>
+      {" "}
+      <div id="header">This Is Header</div>{" "}
+      <div className="bg-primary-subtle p-2 d-flex justify-content-between align-items-center">
+        {" "}
+        <div className="text-primary fs-4 fw-bold">My Company</div>{" "}
+        <div className="d-flex gap-4">
+          {" "}
+          <Link to={"/"}>Home</Link> <Link to={"/about"}>About</Link>{" "}
+          <Link to={"/product"}>Product</Link>{" "}
+          <Link to={"/contact-us"}>Contact Us</Link>{" "}
+        </div>{" "}
+        <div className="d-flex gap-3">
+          {" "}
+          <Link to={"/login"}>
+            {" "}
+            <button className="btn btn-outline-primary">Login</button>{" "}
+          </Link>{" "}
+          <Link to={"/register"}>
+            {" "}
+            <button className="btn btn-primary">Register</button>{" "}
+          </Link>{" "}
+        </div>{" "}
+      </div>{" "}
+    </>
+  );
 }
-
-export default Header;
