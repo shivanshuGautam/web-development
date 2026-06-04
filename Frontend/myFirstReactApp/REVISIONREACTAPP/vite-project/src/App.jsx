9981d687c747ca30839c77765115da95f11e4bd2
shivanshuGautam <harigautam772@gmail.com>
2026-06-04 23:14:03 +0530

    Refactor App component to remove duplicate name displays

--- Frontend/myFirstReactApp/REVISIONREACTAPP/vite-project/src/App.jsx
+++ Frontend/myFirstReactApp/REVISIONREACTAPP/vite-project/src/App.jsx
@@ -2,9 +2,9 @@ function App() {
   return (
     <>
       <div>Home</div>
-      <div className="d-flex justify-content-center align-items-center">Shivanshu Gautam</div>
-      <div className="d-flex justify-content-center align-items-center">Shivanshu Gautam1</div>
-      <div className="d-flex justify-content-center align-items-center">Shivanshu Gautam1</div>
+      <div className="d-flex justify-content-center align-items-center">
+        Shivanshu Gautam
+      </div>
     </>
   );
 }
