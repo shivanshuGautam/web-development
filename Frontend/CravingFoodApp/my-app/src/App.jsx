891ca882519a72bc9d68ad4fe8a25f2cb2be25b7
shivanshuGautam <harigautam772@gmail.com>
2026-06-08 20:08:36 +0530

    feat: integrate Tailwind CSS and React Router
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated React and React DOM to the latest versions.
    - Implemented a basic structure for the App component with routing using React Router.
    - Created a Footer component and included it in the App component.
    - Added an index.css file to import Tailwind CSS styles.
    - Updated Vite configuration to include Tailwind CSS plugin.
    - Created a placeholder Header component file for future use.

---
+++ Frontend/CravingFoodApp/my-app/src/App.jsx
@@ -0,0 +1,24 @@
+// import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      {/* <div className="bg-green-200  text-light p-4">App</div>
+        <button className="bg-blue-400 border border-blue-200">submit</button>
+
+
+        <h1>helloWorld</h1>
+        <h1>helloWorld</h1>
+        <h1>helloWorld</h1> */}
+
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ Frontend/CravingFoodApp/my-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <>
+      <div id="footer">This Is Footer</div>
+    </>
+  );
+}
+
+export default Footer;
