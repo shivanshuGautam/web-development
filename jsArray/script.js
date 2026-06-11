cb19f49cb53e20c1f7dbb156b2647db707284a0d
shivanshuGautam <harigautam772@gmail.com>
2026-06-11 23:47:47 +0530

    Refactor comments and formatting for clarity in array operations

--- jsArray/script.js
+++ jsArray/script.js
@@ -1,37 +1,37 @@
-// // initial array
-// let arr = [12, 13, 45, 67, 89, 34];
-// for (var i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
-// console.log(arr);
+// initial array
+let arr = [12, 13, 45, 67, 89, 34];
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+console.log(arr);
 
-// // update
-// arr[2] = arr[2] + 10;
-// console.log(arr);
+// update
+arr[2] = arr[2] + 10;
+console.log(arr);
 
-// // delete (empty array)
-// arr = [];
-// console.log(arr);
+// delete (empty array)
+arr = [];
+console.log(arr);
 
-// // push
-// arr = [12, 89, 76, 56, 45, 89, 54, 34, 78, 90];
-// console.log(arr);
-// arr.push(99);
-// console.log(arr);
+// push
+arr = [12, 89, 76, 56, 45, 89, 54, 34, 78, 90];
+console.log(arr);
+arr.push(99);
+console.log(arr);
 
-// // pop
-// arr.pop();
-// console.log(arr);
+// pop
+arr.pop();
+console.log(arr);
 
-// // unshift
-// arr.unshift(11);
-// console.log(arr);
+// unshift
+arr.unshift(11);
+console.log(arr);
 
-// // shift
-// arr.shift();
-// console.log(arr);
+// shift
+arr.shift();
+console.log(arr);
 
-// short
+short
 
 let unar = [12, 65, 9, 43, 65, 73, 24];
 unar.sort;
