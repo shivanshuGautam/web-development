3565b102418cd85af9cfe559c3846b5cccc4edd0
shivanshuGautam <harigautam772@gmail.com>
2026-06-15 22:49:59 +0530

    Change Navbar text color from white to black for better visibility

--- Frontend/CravingFoodApp/my-app/src/components/Navbar.jsx
+++ Frontend/CravingFoodApp/my-app/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="bg-[var(--primary)] text-white">Navbar</div>;
+  return <div className="bg-[var(--primary)] text-black">Navbar</div>;
 };
 
 export default Navbar;
